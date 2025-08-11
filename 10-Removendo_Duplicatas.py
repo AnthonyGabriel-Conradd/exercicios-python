69aefef3a387f2d11acaf9e5283b61718b33e726
AnthonyGabriel-Conradd <lvcpj2016@gmail.com>
2025-08-11 18:53:04 -0300

    Create 10-Removendo_Duplicatas.py
    solução concluída do RemovendoDuplicatas

---
+++ 10-Removendo_Duplicatas.py
@@ -0,0 +1,7 @@
+def remover_duplicatas(lista):
+    """Retorna uma nova lista sem valores duplicados."""
+    return list(set(lista))
+
+if __name__ == "__main__":
+    numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+    print(remover_duplicatas(numeros))
