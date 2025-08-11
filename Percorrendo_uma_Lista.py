79259eaf227cb162822157c0759dfeb02847beab
AnthonyGabriel-Conradd <lvcpj2016@gmail.com>
2025-08-11 18:28:21 -0300

    Create Percorrendo_uma_Lista.py
    Solução Concluida

---
+++ Percorrendo_uma_Lista.py
@@ -0,0 +1,8 @@
+def percorrer_lista(lista):
+    """Imprime cada elemento da lista."""
+    for item in lista:
+        print(item)
+
+if __name__ == "__main__":
+    numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+    percorrer_lista(numeros)
