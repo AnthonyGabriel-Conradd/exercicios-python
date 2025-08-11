48310e48723597a0b467eea051e518de5e638e71
AnthonyGabriel-Conradd <lvcpj2016@gmail.com>
2025-08-11 18:49:39 -0300

    Create 8-Somando_os_Valores_da_Lista.py
    solução concluída do SomandoosValoresdaLista

---
+++ 8-Somando_os_Valores_da_Lista.py
@@ -0,0 +1,7 @@
+def somar_valores(lista):
+    """Retorna a soma de todos os valores da lista."""
+    return sum(lista)
+
+if __name__ == "__main__":
+    numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+    print(somar_valores(numeros))
